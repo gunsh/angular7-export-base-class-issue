8151f0372e55c8f5b7b51dc64f49ca31bc8adb09
Aleksandar Puskas <apuskas@gunsh.com>
2019-04-23 17:39:19 +0200

    Changes to demonstrate the bug.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { NgModule, Component, Input } from '@angular/core';
+
+// Uncomment "export" below to fix the bug
+//export
+class BaseComponent {
+  @Input() txt: string = "nothing";
+}
+
+@Component({
+  selector: '[derived]',
+  template: `<p>{{txt}}</p>`
+})
+export class DerivedComponent extends BaseComponent {
+
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div derived [txt]="'hello world!'"></div>`
 })
 export class AppComponent {
-  title = 'repro-app';
+}
+
+@NgModule({
+  declarations: [
+    DerivedComponent
+  ],
+  exports: [
+    DerivedComponent
+  ]
+})
+export class MyModule {
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, MyModule } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
